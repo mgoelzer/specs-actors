c7b0eadba7e7305d2c145a5d12a2a2af0ae19f03
Anton Evangelatov <anton.evangelatov@gmail.com>
2020-07-16 18:45:18 +0200

    try to reduce again window to 5

--- actors/builtin/miner/policy_params_testground.go
+++ actors/builtin/miner/policy_params_testground.go
@@ -7,10 +7,10 @@ import (
 )
 
 // The period over which all a miner's active sectors will be challenged.
-const WPoStProvingPeriod = abi.ChainEpoch(360) // proving period set to 36*3 epochs ~ 6min. instead of 24 hours (assuming block time delay of 1sec.)
+const WPoStProvingPeriod = abi.ChainEpoch(180) // proving period set to 36*3 epochs ~ 6min. instead of 24 hours (assuming block time delay of 1sec.)
 
 // The duration of a deadline's challenge window, the period before a deadline when the challenge is available.
-const WPoStChallengeWindow = abi.ChainEpoch(10) // challenge window set to 10 epochs ~ 10sec. instead of 40 minutes (assuming block time delay of 1sec.)
+const WPoStChallengeWindow = abi.ChainEpoch(5) // challenge window set to 10 epochs ~ 10sec. instead of 40 minutes (assuming block time delay of 1sec.)
 
 // The number of non-overlapping PoSt deadlines in each proving period.
 const WPoStPeriodDeadlines = uint64(WPoStProvingPeriod / WPoStChallengeWindow) // 30 periods (one period every 10 epochs?)
